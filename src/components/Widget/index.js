b50991f54ba86ac6cca9a0c48d85a943f56768fc
Pawel Peczkowski <peczkowskip@gmail.com>
2020-05-15 01:07:03 +0200

    [#6] control drag & scroll

--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,77 +1,12 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+import React from 'react'
 
+import { WidgetView } from '../styled'
 import WidgetWindow from './widgetWindow'
 
-const WidgetView = styled.div`
-	position: relative;
-	width: 700px;
-	height: auto;
-	border-left: 1px solid grey;
-	transform: translateZ(0);
-
-	&:hover {
-		cursor: grab;
-	}
-
-	&::before {
-		content: '';
-		position: absolute;
-		width: 0px;
-		height: 100%;
-		left: 0px;
-		top: 0px;
-		box-shadow: 2px 0px 10px 3px #aaaaaa;
-		z-index: 10;
-	}
-
-	&::after {
-		content: '';
-		position: absolute;
-		top: 0px;
-		right: 0px;
-		height: 100%;
-		box-shadow: -5px 0px 60px 35px #ffffff;
-		z-index: 10;
-	}
-`
-
-const NavigationArrow = styled.div`
-	position: fixed;
-	top: 40%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: 40px;
-	height: 80px;
-	background-color: #f0f0f0;
-	opacity: ${props => props.show ? '0.6' : '0'};
-	transition: all 0.5s ease;
-`
-const ArrowLeft = styled(NavigationArrow)`
-	left: 0px;
-	border-bottom-right-radius: 90px;
-	border-top-right-radius: 90px;
-	z-index: 11;
-`
-
-const ArrowRight = styled(NavigationArrow)`
-	right: 0px;
-	border-bottom-left-radius: 90px;
-	border-top-left-radius: 90px;
-	z-index: 11;
-`
-
 const widgetView = () => {
-	const [show, setShow] = useState(false)
-
-	const showNavigation = () => setShow(true)
-	const hideNavigation = () => setShow(false)
 
 	return (
-		<WidgetView onMouseEnter={showNavigation} onMouseLeave={hideNavigation}>
-			<ArrowLeft show={show}>left</ArrowLeft>
-			<ArrowRight show={show}>right</ArrowRight>
+		<WidgetView>
 			<WidgetWindow/>
 		</WidgetView>
 	)
--- src/components/styled/index.js
+++ src/components/styled/index.js
@@ -1,3 +1,4 @@
 export { LegendContainer, LegendItem } from './Legend/'
 export { Box, IconImg } from './utils'
 export { RangeContainer, Point, Label } from './Widget/Box/RangeBox'
+export { WidgetView, ArrowLeft, ArrowRight} from './Widget'
